b45c17d8d91f6056397160a5d68737df5135a8b2
Mohamed-Nasr55 <mohamed.nasr.ali998855@gmail.com>
2025-11-02 12:01:01 +0200

    - created development branch

---
+++ lib/e_commerce_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ECommerceApp extends StatelessWidget {
+  const ECommerceApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'E-Commerce App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:ecommerceapp/e_commerce_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ECommerceApp());
+}
+
+
+
