8aa683d6c6f840102dd61e6ebce596767c4beb93
Mohamed-Nasr55 <mohamed.nasr.ali998855@gmail.com>
2025-11-02 13:36:34 +0200

    - added helper folder and extension for sized box

---
+++ lib/core/helpers/extensions.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+extension SizedBoxExtension on num {
+  SizedBox get height => SizedBox(height: toDouble().h);
+  SizedBox get width => SizedBox(height: toDouble().w);
+}
